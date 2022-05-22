70f342c4d7cf9aeb2c36f27108cb6421384cd91b
Jaron van Well <49275554+JaronVW@users.noreply.github.com>
2022-05-22 13:39:26 +0200

    connections are made and closed per test file to prevent endless load

--- test/integration/user-controller-get.test.js
+++ test/integration/user-controller-get.test.js
@@ -2,7 +2,19 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { init } = require("../../src/index");
 const app = require("../../src/index");
-const Database = require("../../src/testdb");
+require("dotenv").config();
+let mysql = require("mysql2");
+
+
+const Database = mysql.createConnection({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  multipleStatements: true
+});
 const { describe, it, beforeEach } = require("mocha");
 const { should } = require("chai");
 const JWT = require("jsonwebtoken");
@@ -211,7 +223,10 @@ describe("Get users", () => {
       });
   });
 
-  
+  after(function(done) {
+    Database.end();
+    done();
+   }); 
   
   
 });
